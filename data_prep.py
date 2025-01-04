7e0c6a07142fa869468044e413f86ef3fe0e2cfe
maman510 <153794692+maman510@users.noreply.github.com>
2025-01-04 14:39:14 -0800

    UPdated data_prep

--- data_prep.py
+++ data_prep.py
@@ -1,6 +1,5 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import tool_box
@@ -10,6 +9,23 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np 
 import random
+import soundfile as sf
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+def play_audio(wav_file_path):
+    
+    if os.path.exists(wav_file_path) == False:
+        print(tool_box.color_string('red', f'\nFILE {wav_file_path} DOES NOT EXIST\n'))
+        return None
+    else:
+        print(tool_box.color_string('green', f'\n\n\tPLAY: {wav_file_path}\n\n'))
+        audio = AudioSegment.from_wav(wav_file_path)
+        play(audio)
+        return None
+
+
 
 def load_vctk_dataset(batch_size=256, load_all=False):
 
@@ -75,6 +91,7 @@ def load_vctk_dataset(batch_size=256, load_all=False):
 
         
     '''
+    
 @tool_box.timer  
 def create_audio_files(sample_size=10000):
     print(tf.config.list_physical_devices('GPU'))
@@ -111,10 +128,27 @@ def create_mel_spectrogram(audio_path, save_path):
         audio_data = tool_box.Load_Pkl(audio_path).astype("float32")
         sample_rate = 22050
 
-        S = np.abs(librosa.stft(audio_data, n_fft=4096))**2
-        chroma = librosa.feature.chroma_stft(S=S, sr=sample_rate)
-        S_db = librosa.power_to_db(S=chroma, ref=np.max)
-        img = librosa.display.specshow(S_db, sr=sample_rate, fmax=8000)
+        # S = np.abs(librosa.stft(audio_data, n_fft=4096))**2
+        # chroma = librosa.feature.chroma_stft(S=S, sr=sample_rate)
+        # S_db = librosa.power_to_db(S=chroma, ref=np.max)
+        # img = librosa.display.specshow(S_db, sr=sample_rate, fmax=8000)
+        S = librosa.feature.melspectrogram(y=audio_data,
+                                           sr=sample_rate, 
+                                            n_fft=2048, 
+                                            hop_length=512, 
+                                            win_length=None, 
+                                            window='hann', 
+                                            center=True, 
+                                            pad_mode='reflect', 
+                                            power=2.0,
+                                            n_mels=128)
+        S_db = librosa.power_to_db(S, ref=np.max)
+        img = librosa.display.specshow(S_db, sr=sample_rate)
+        
+        #S = librosa.feature.melspectrogram(y=audio_data, sr=sample_rate, n_mels=128, fmax=8000)
+       # S_db = librosa.power_to_db(S, ref=np.max)
+       # img = librosa.display.specshow(S_db, sr=sample_rate, fmax=8000)
+
 
         plt.savefig(save_path, transparent = True, pad_inches = 0, bbox_inches="tight")
         plt.close()
@@ -133,7 +167,7 @@ def load_image(path, resize_dims=(512,512)):
  
     return loaded
 
-def resize_all_images():
+def resize_all_images(resize_dims=(512,512)):
 
     def check_results(good_images, img_paths):
         user_input_prompt = tool_box.color_string('yellow', f"\n\nALL FILES SUCCESSFULLY RESIZED; DELETE ORIGINAL DATASET AND SET NEW DATASET TO RESIZED?\n ('y' or 'n')\n\n")
@@ -160,7 +194,7 @@ def resize_all_images():
         print(tool_box.color_string('red', f"\nTEMP IMAGE FILE EXISTS ALREADY; DELETE OR CHANGE NAME TO CONTINUE\n"))
         return None
     else:
-        print(f"\nRESIZING IMAGES...\n")
+        print(f"\nRESIZING IMAGES to {resize_dims}...\n")
         os.mkdir(f"{os.getcwd()}/temp_images")
         image_paths = [f"{os.getcwd()}/image_data/{f}" for f in os.listdir("image_data")]
         resized_image_paths = [f"{os.getcwd()}/temp_images/{f}".strip("_ORIGINAL.png") + "_resized.png" for f in os.listdir("image_data")]
@@ -172,7 +206,7 @@ def resize_all_images():
                     
                 original_path = image_paths[i]
                 resize_path = resized_image_paths[i]
-                resized_original = load_image(original_path)
+                resized_original = load_image(original_path,  resize_dims=resize_dims)
                 resized_original.save(resize_path)
                 width, height = resized_original.size
                 msg = tool_box.color_string('green', f'\nADDED: {resize_path} from {original_path}; new dims: ({width}, {height})\t') + tool_box.color_string('yellow', f'({count+1} out of {len(image_paths)})\n')
@@ -326,3 +360,98 @@ def create_training_dirs(test_split=0.1):
     total_count = train_count + test_count
     print("found count " , total_count )
 
+
+
+
+
+def create_wav_file(audio_data_path, save_path, sr=44100):
+    '''
+        accepts audio_data_path as str or as a vector of signal data
+    '''
+
+    if type(audio_data_path) == str:
+        audio = tool_box.Load_Pkl(audio_data_path).astype("int16")
+    else:
+        audio  = audio_data_path.astype("int16")
+  
+    sf.write(save_path, audio, samplerate=sr)
+    print(tool_box.color_string('green',f"ADDED AUDIO: {save_path}"))
+    return None
+    
+
+def create_mel_audio(mel_image_path, resize_dim=(32,32), sample_rate=44100):
+
+    #load mel image and reshape 
+    mel_img =  np.array(Image.open(mel_image_path).convert("RGB")).astype("float64")
+
+    #conver img to audio
+    mel_audio = librosa.feature.inverse.mel_to_audio(mel_img,
+                                            sr=sample_rate, 
+                                            n_fft=2048, 
+                                            hop_length=512, 
+                                            win_length=None, 
+                                            window='hann', 
+                                            center=True, 
+                                            pad_mode='reflect', 
+                                            power=2.0).astype("int16").flatten()
+
+    mel_audio_save_path = f'{os.getcwd()}/{mel_image_path.split("/")[-1].split(".png")[0]}_mel_audio.wav'
+    return create_wav_file(mel_audio, mel_audio_save_path)
+    # sf.write(mel_audio_path, mel_audio, samplerate=sample_rate)
+    # print(tool_box.color_string('green', f'\nADDED: {mel_audio_path}\n'))
+
+
+def create_all_wav_files():
+
+    audio_paths = [f"{os.getcwd()}/audio_data/{f}" for f in os.listdir(f"{os.getcwd()}/audio_data")]
+    save_paths = [f"{os.getcwd()}/wav_data/{f.split('.pkl')[0]}.wav" for f in os.listdir(f"{os.getcwd()}/audio_data")]
+    count = 0
+    for i in range(len(audio_paths)):
+        audio_path = audio_paths[i]
+        save_path = save_paths[i]
+        try:
+            create_wav_file(audio_path, save_path, sr=44100)
+            count += 1
+            print(tool_box.color_string('green',f"\nADDED: {save_path} ({count} out of {len(audio_paths)})\n"))
+        except Exception as e:
+            count += 1
+            print(tool_box.color_string('red',f"\nFAILED: {save_path}; {e} ({count} out of {len(audio_paths)})\n"))
+
+
+
+def load_and_preprocess_image(path):
+    '''
+        EXAMPLE USE CASE FOR THIS MAPPING FUNCTION:
+        
+        #load paths
+        paths = [f"{os.getcwd()}/train_session_data/male/{f}" for f in os.listdir(f"{os.getcwd()}/train_session_data/male")]
+
+        # Create a dataset from the image paths
+        dataset = tf.data.Dataset.from_tensor_slices(paths)
+
+        # Map the dataset to load and preprocess images
+        dataset = dataset.map(load_and_preprocess_image)
+
+        # Create a tensor from the dataset
+        X_train = tf.convert_to_tensor(list(dataset))
+        test_images = X_train[100:110]
+
+    '''
+    # Load the image
+    image = tf.io.read_file(path)    
+    image = tf.image.decode_jpeg(image, channels=3)
+    # # Preprocess the image (resize, normalize, etc.)
+    image = tf.image.resize(image, [32, 32])
+
+    image = tf.cast(image, tf.float32) / 255.0 
+    return image
+
+
+
+
+paths = [f"{os.getcwd()}/wav_data/{f}" for f in os.listdir(f"{os.getcwd()}/wav_data")]
+for i in range(10):
+
+    index = random.randint(0,len(paths) - 1)
+    path = paths[index]
+    play_audio(path)
